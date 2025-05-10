39478eaa54a037337ab388e2c83fc20d95b70b6c
sreeni.reddy <sreenivasareddysaddikuti@localhost>
2025-05-10 08:32:45 +0530

    testing1

--- src/main/java/com/sreeni/App.java
+++ src/main/java/com/sreeni/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println("Adding code from ec");
         System.out.print("tesintg from EC");
         System.out.print("tesintg from EC");
+        System.out.print("tesintg from EC1");
 
     }
 
